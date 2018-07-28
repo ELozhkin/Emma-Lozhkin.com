1bfc77c31cfb6a93053fa41c51c4de6367b57bf6
ELozhkin <35240686+ELozhkin@users.noreply.github.com>
2018-07-28 17:23:08 -0400

    navbar correctly navigates to website sections

--- mainJS.js
+++ mainJS.js
@@ -26,6 +26,19 @@ function wordtoggle(x){
 // ============== arrows to change sections
 
 
+// ========== navbar navigation to different sections
+function scrollto(div){
+	var extra = 0;
+	if (div === 'one') {extra = 60}
+	else if (div === 'two') {extra = 130}
+	else if (div === 'three') {extra = 150}
+	else if (div === 'four') {extra = 150}
+	$('html,body').animate({
+		scrollTop: $("#"+div).offset().top - extra
+	},'slow');
+	
+};
+
 
 // ========== sticky navbar on scroll 
 window.onscroll = function() {myFunction()};
@@ -44,7 +57,7 @@ function myFunction() {
   var y = $(this).scrollTop();
       
       // If the document is scrolled 90%
-      if( y > 550) {
+      if( y > 500) {
         // Add the "sticky" class
         $('nav').addClass('sticky');
         $('nav').addClass('visi');
